932531fa8e47e37d1ef6909dc60c8043ad0074a7
C4rtex <carlos.jimenezc@cun.edu.co>
2021-09-20 17:48:11 -0500

    Hola:v

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+const typed = new Typed('.typed', {
+    strings: ['Calidad',
+        'Presupuesto',
+        'Tradición <i>;)</i>',
+    ],
+
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    backDelay: 500,
+    contentType: 'html',
+});
